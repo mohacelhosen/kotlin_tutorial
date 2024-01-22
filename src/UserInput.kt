c6457257e6a1d83f3ff3ee06d04ae5cc91de3a22
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-22 15:44:48 +0600

    user input, safe call and elvis operator

--- src/UserInput.kt
+++ src/UserInput.kt
@@ -1,4 +1,12 @@
 
+/*
+readlnOrNull = it returns null if there is an issue or if the input is empty otherwise return  input stream(user input)
+safe call operator(?.) = only accessible on the right side function() when it's not null; if it's null, then the right side function() will never be executed.
+elvis operator (?:) = It is used to provide a default value when the expression on its left side is null.
+
+    val result = expressionThatMightBeNull ?: defaultValue
+
+ */
 fun main(){
     print("Enter number:: ")
     val userInput = readlnOrNull()?.toIntOrNull()?:0

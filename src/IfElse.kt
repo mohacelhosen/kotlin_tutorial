f66557811ea2065f8d6a4e56b31eb51707433c37
mohacelhosen <mohacel.hosen22@gmail.com>
2024-01-22 14:05:36 +0600

    if, else if and else

---
+++ src/IfElse.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var age = 18;
+    if (age>12 && age<=19){
+        println("Teenager")
+    }else if(age>19 && age <= 45){
+        println("Adult")
+    }else{
+        println("Old")
+    }
+}
